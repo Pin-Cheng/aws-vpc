c333bcbd6f175303ae50f4a437a5e643d13c0eeb
leo.chen <leo.chen@wonderland.com.tw>
2023-02-03 14:24:53 +0800

    aws vpc tf

---
+++ vpc-demo-2/main.tf
@@ -0,0 +1,37 @@
+resource "aws_vpc" "main" {
+ cidr_block = "10.0.0.0/16"
+ 
+ tags = {
+   Name = "Project VPC-1"
+ }
+}
+
+resource "aws_subnet" "public_subnets" {
+ count      = length(var.public_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.public_subnet_cidrs, count.index)
+ availability_zone = element(var.azs, count.index)
+ 
+ tags = {
+   Name = "Public Subnet-1 ${count.index + 1}"
+ }
+}
+ 
+resource "aws_subnet" "private_subnets" {
+ count      = length(var.private_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.private_subnet_cidrs, count.index)
+ availability_zone = element(var.azs, count.index)
+ 
+ tags = {
+   Name = "Private Subnet-1 ${count.index + 1}"
+ }
+}
+
+# resource "aws_internet_gateway" "gw" {
+#  vpc_id = aws_vpc.main.id
+ 
+#  tags = {
+#    Name = "Project VPC IG"
+#  }
+# }
