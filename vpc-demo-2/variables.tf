e549aac144ca092fc90864b1854d4f724a58312c
leo.chen <leo.chen@wonderland.com.tw>
2023-02-04 14:53:03 +0800

    2 tf cahnge test

--- vpc-demo-2/variables.tf
+++ vpc-demo-2/variables.tf
@@ -5,13 +5,13 @@ variable "region" {
 variable "public_subnet_cidrs" {
  type        = list(string)
  description = "Public Subnet CIDR values"
- default     = ["10.2.1.0/24", "10.2.2.0/24", "10.2.3.0/24"]
+ default     = ["10.20.1.0/24", "10.20.2.0/24", "10.20.3.0/24"]
 }
  
 variable "private_subnet_cidrs" {
  type        = list(string)
  description = "Private Subnet CIDR values"
- default     = ["10.2.4.0/24", "10.2.5.0/24", "10.2.6.0/24"]
+ default     = ["10.20.4.0/24", "10.20.5.0/24", "10.20.6.0/24"]
 }
 
 variable "azs" {
