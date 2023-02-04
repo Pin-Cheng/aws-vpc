2212d0aa05e39f381e7a445de439087bb4269847
leo.chen <leo.chen@wonderland.com.tw>
2023-02-04 14:17:54 +0800

    create two demo igw

--- vpc-demo/main.tf
+++ vpc-demo/main.tf
@@ -38,10 +38,10 @@ resource "aws_subnet" "private_subnets" {
  }
 }
 
-# resource "aws_internet_gateway" "gw" {
-#  vpc_id = aws_vpc.main.id
+resource "aws_internet_gateway" "gw" {
+ vpc_id = aws_vpc.main.id
  
-#  tags = {
-#    Name = "Project VPC IGW Demo"
-#  }
-# }
+ tags = {
+   Name = "Project VPC IGW Demo"
+ }
+}
